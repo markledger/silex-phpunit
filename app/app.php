d0146e85e263f724cb89935a15f66b99382023e6
Mark Ledger <mark_ledger@outlook.com>
2016-08-16 15:19:28 +0100

    Created environment variable

--- app/app.php
+++ app/app.php
@@ -6,6 +6,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app = new Silex\Application();
 
+// Detect environment (default: prod) by checking for the existence of $app_env
+if (isset($app_env) && in_array($app_env, array('prod','dev','test')))
+    $app['env'] = $app_env;
+else
+    $app['env'] = 'prod';
+
 
 $app['debug'] = true;
 
